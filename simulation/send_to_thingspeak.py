8e098d81f7b6197ddbf881c4d6d2cf6beb894a02
360Chagona <bsc-15-18@unima.ac.mw>
2023-05-16 15:35:30 -0700

    added sending data to firebase from proteus

--- simulation/send_to_thingspeak.py
+++ simulation/send_to_thingspeak.py
@@ -1,33 +1,18 @@
 import serial
 import requests
 import time
-# import pyrebase 
-
-
-
-# config = {
-#     "apiKey": "AIzaSyC_H_6B3wCiLmDcO8sJosRjitowHSvDfY8",
-#     "authDomain": "smart-prepaid-water-meter.firebaseapp.com",
-#     "databaseURL": "https://smart-prepaid-water-meter-default-rtdb.firebaseio.com",
-#     "projectId": "smart-prepaid-water-meter",
-#     "storageBucket": "smart-prepaid-water-meter.appspot.com",
-#     "messagingSenderId": "97974428599",
-#     "appId": "1:97974428599:web:62d111583ade730ff07e32",
-#     "measurementId": "G-1TB2Y5W79B"
-# }
-
-# firebase = pyrebase.initiallise_app(config)
-# database = firebase.database()
-# Set up serial port
-ser = serial.Serial('COM1', 9600)  # Replace 'COM1' with the appropriate port name on your system
 
 # ThingSpeak channel details
 channel_id = '2137933'
 write_api_key = 'KM46GR8219E7Z7ZL'
 
-# Write integer value 10 to the serial port
-# ser.write(b'10')
+# Firebase Realtime Database details
+firebase_url = 'https://proteus-data-default-rtdb.firebaseio.com/'
+firebase_key = 'AIzaSyAJo32NZ-dDL0SXATwD9TzKCWmS0_1pW30'
 
+# Set up serial port
+ser = serial.Serial('COM1', 9600)  # Replace 'COM1' with the appropriate port name on your system
+ser.write(b'10')
 while True:
     # Read the results from the serial port
     data = ser.readline().strip().decode('utf-8')
@@ -40,11 +25,28 @@ while True:
         continue
 
     # Send data to ThingSpeak
-    url = f'https://api.thingspeak.com/update?api_key={write_api_key}&field1={total_millilitres}&field2={flow_millilitres}&field3={current_volume}'
-    response = requests.get(url)
+    ts_url = f'https://api.thingspeak.com/update?api_key={write_api_key}&field1={total_millilitres}&field2={flow_millilitres}&field3={current_volume}'
+    ts_response = requests.get(ts_url)
 
-    print('Data sent to ThingSpeak:', total_millilitres, flow_millilitres, current_volume)
+    # Create the data payload for Firebase
+    firebase_data = {
+        'total_millilitres': total_millilitres,
+        'flow_millilitres': flow_millilitres,
+        'current_volume': current_volume
+    }
+
+    # Send data to Firebase
+    fb_url = f'{firebase_url}.json?auth={firebase_key}'
+    fb_response = requests.patch(fb_url, json=firebase_data)
+
+    if ts_response.status_code == 200 and fb_response.status_code == 200:
+        print('Data sent to ThingSpeak and Firebase:', total_millilitres, flow_millilitres, current_volume)
+    else:
+        print('Failed to send data. ThingSpeak status:', ts_response.status_code, 'Firebase status:', fb_response.status_code)
 
     time.sleep(1)  # Wait for 1 second before reading the next data from the serial port
-    # Sending data to the firebase
-    # database.push({"meter/Volume":total_millilitres, "meter/flowRate":flow_millilitres})
+
+
+
+
+
