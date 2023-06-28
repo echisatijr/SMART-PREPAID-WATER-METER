49e096b038be5d78e431fa3ba6b3da80a08ecd66
360Chagona <bsc-15-18@unima.ac.mw>
2023-06-28 22:28:02 +0200

    app

--- dashboard-react/src/components/alert/Notification.jsx
+++ dashboard-react/src/components/alert/Notification.jsx
@@ -3,8 +3,25 @@ import './Notification.css'
 import { Alert, Button, Stack } from '@mui/material'
 
 const Notification = ({ data }) => {
-  const { token } = data || {} // Extract the token value from the data object
+  const { token, volume } = data || {} // Extract the token value from the data object
   const tok = token && token.key // Assign the token value to the 'tok' variable
+  const volumeWater = volume && volume.key // Assign the token value to the 'tok' variable
+  console.log('volume data', volume)
+
+  const calculateRemainingWaterPercentage = (tok) => {
+    // Assuming the total water capacity is 1000 mL
+
+    const volume = volumeWater
+    const remainingWaterPercentage = (volume / tok) * 100
+    if (remainingWaterPercentage != 'NaN') {
+      return remainingWaterPercentage.toFixed(0) // Round the percentage to two decimal places
+    } else {
+      console.log('value is NaN')
+    }
+  }
+
+  const remainingWaterPercentage = calculateRemainingWaterPercentage(tok)
+  console.log('The remaining water is ', remainingWaterPercentage, '%')
 
   const [values, setValues] = useState(() => {
     // Retrieve the stored values from browser storage
@@ -12,8 +29,30 @@ const Notification = ({ data }) => {
     return storedValues ? JSON.parse(storedValues) : []
   })
 
+  const [remwater, setRemwater] = useState(() => {
+    const storedRemwater = localStorage.getItem('remainingWater')
+    return storedRemwater ? JSON.parse(storedRemwater) : []
+  })
+
   useEffect(() => {
-    if (tok) {
+    if (
+      remainingWaterPercentage &&
+      !remwater.includes(remainingWaterPercentage)
+    ) {
+      const newRem = [...remwater, remainingWaterPercentage]
+      setRemwater(newRem)
+
+      localStorage.setItem('remainingWater', JSON.stringify(newRem))
+    }
+  })
+  useEffect(() => {
+    const newRem = localStorage.getItem('remainingWater')
+    if (newRem) {
+      setValues(JSON.parse(newRem))
+    }
+  }, [])
+  useEffect(() => {
+    if (tok && !values.includes(tok)) {
       const newValues = [...values, tok]
       setValues(newValues)
       // Store the updated values in browser storage
@@ -21,7 +60,23 @@ const Notification = ({ data }) => {
     }
   }, [tok])
 
+  useEffect(() => {
+    const storedValues = localStorage.getItem('notificationValues')
+    if (storedValues) {
+      setValues(JSON.parse(storedValues))
+    }
+  }, [])
+
+  const clearValues = () => {
+    setValues([])
+    setRemwater([])
+    // Clear the stored values from browser storage
+    localStorage.removeItem('notificationValues')
+    localStorage.removeItem('remainingWater')
+  }
+
   console.log('array not', values)
+  console.log('array remwater', remwater)
 
   return (
     <div className='noti'>
@@ -33,20 +88,17 @@ const Notification = ({ data }) => {
                 You have successfully purchased {value} mL
               </Alert>
             ))}
+            {['50', '70', '90', '100'].includes(remainingWaterPercentage) && (
+              <Alert className='alert-not' severity='warning'>
+                You have used {remainingWaterPercentage} % of your water units
+              </Alert>
+            )}
           </Stack>
         </div>
       </div>
 
       <div className='btn'>
-        <Button
-          className='notbtn'
-          variant='contained'
-          onClick={() => {
-            setValues([])
-            // Clear the stored values from browser storage
-            localStorage.removeItem('notificationValues')
-          }}
-        >
+        <Button className='notbtn' variant='contained' onClick={clearValues}>
           Clear All
         </Button>
       </div>
